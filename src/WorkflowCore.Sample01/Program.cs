fe2d27237a7a8743a261995ed194ed105e34d69f
josenerydev <josenerydev@gmail.com>
2025-02-13 21:02:20 -0300

    Add project files.

---
+++ src/WorkflowCore.Sample01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using WorkflowCore.Interface;
+using WorkflowCore.Sample01;
+using WorkflowCore.Sample01.Steps;
+
+
+var serviceProvider = ConfigureServices();
+var host = serviceProvider.GetService<IWorkflowHost>();
+host.RegisterWorkflow<HelloWorldWorkflow>();
+host.Start();
+await host.StartWorkflow("HelloWorld");
+
+Console.ReadLine();
+host.Stop();
+return;
+
+static IServiceProvider ConfigureServices()
+{
+    IServiceCollection services = new ServiceCollection();
+    services.AddLogging();
+    services.AddWorkflow();
+
+    services.AddTransient<GoodbyeWorld>();
+
+    var serviceProvider = services.BuildServiceProvider();
+
+    return serviceProvider;
+}
