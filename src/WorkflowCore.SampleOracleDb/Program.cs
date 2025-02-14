ddac957d3912427624605ca614e8ecba9f7fcd3d
josenerydev <josenerydev@gmail.com>
2025-02-14 08:52:57 -0300

    Add WorkflowCore.SampleOracleDb project and API
    This commit introduces a new project `WorkflowCore.SampleOracleDb` to the solution, including:
    - A `WeatherForecastController` for weather data retrieval.
    - A `Dockerfile` for building and running the application in .NET 8.0.
    - Updates to `Program.cs` for Oracle database persistence configuration.
    - Modifications to `launchSettings.json` for Docker support.
    - A new model class `WeatherForecast`.
    - Creation of `WorkflowCore.SampleOracleDb.csproj` with necessary package references.
    - An HTTP request file for API testing.
    - Configuration files for logging and allowed hosts.

---
+++ src/WorkflowCore.SampleOracleDb/Program.cs
@@ -0,0 +1,31 @@
+using WorkflowCore.Persistence.Oracle;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Adiciona o serviço de Workflow usando Oracle
+builder.Services.AddWorkflow(x =>
+    x.UseOracle(@"Server=127.0.0.1;Database=workflow;User=root;Password=password;", true, true)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
